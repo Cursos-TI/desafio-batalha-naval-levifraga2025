f1dd971f6ac085375a0d2b76f8a5a72f80a60fc5
levifraga2025 <jrtagima@gmail.com>
2025-09-04 17:05:17 +0000

    inicio batalha Naval

---
+++ batalha_Naval_Novato.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define TAM_TABULEIRO 10  // Tamanho do tabuleiro
+#define TAM_NAVIO 3       // Tamanho fixo dos navios
+
+int main() {
+    int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO] = {0}; // Inicializa o tabuleiro com 0 (água)
